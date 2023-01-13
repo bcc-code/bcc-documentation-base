90d4173727b4f13f4b6b7fd18a10a9ae5c08a9dc
Laurens Groeneveld <laurens.groeneveld@bcc.no>
2023-01-13 10:19:08 +0100

    Add custom 404 page

---
+++ vuepress/docs/.vuepress/theme/layouts/NotFound.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="notfound-container">
+        <img src="/logomark.svg" />
+
+        <h1>404 Not Found</h1>
+
+        <p>Sorry, this page doesn't exist. If you followed a link in the documentation to get here, feel free to update that file in GitHub.</p>
+
+        <div style="margin-bottom: 10px"><RouterLink to="/">Developer Portal →</RouterLink></div>
+    </div>
+</template>
--- vuepress/docs/.vuepress/theme/theme.ts
+++ vuepress/docs/.vuepress/theme/theme.ts
@@ -7,7 +7,10 @@ const __dirname = getDirname(import.meta.url);
 export const bccCodeTheme = (options: DefaultThemeOptions): Theme => {
   return {
     name: 'vuepress-theme-bcc-code',
+
     extends: defaultTheme(options),
+
+    clientConfigFile: path.resolve(__dirname, './client.ts'),
 
     alias: {
       '@theme/HomeFeatures.vue': path.resolve(__dirname, './components/HomeFeatures.vue'),
