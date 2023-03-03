71a83f2980769a85e8722ddf08002c35aaeeb391
Reng van Oord <reng.vanoord@bcc.no>
2023-03-03 11:15:58 +0100

    Trigger update

--- vuepress/docs/.vuepress/theme/layouts/NotFound.vue
+++ vuepress/docs/.vuepress/theme/layouts/NotFound.vue
@@ -4,7 +4,7 @@
 
         <h1>404 Not Found</h1>
 
-        <p>Sorry, this page doesn't exist. If you followed a link in the documentation to get here, feel free to update that file in GitHub.</p>
+        <p>Sorry - this page doesn't exist. If you followed a link in the documentation to get here, feel free to update that file in GitHub.</p>
 
         <div style="margin-bottom: 10px"><RouterLink to="/">Developer Portal →</RouterLink></div>
     </div>
