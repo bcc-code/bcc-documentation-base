2773f5b50f99ec013ab479c06531eac53436bfc0
Reng van Oord <reng.vanoord@bcc.no>
2023-03-03 10:46:22 +0100

    Fixed notfound handling

--- vuepress/docs/.vuepress/theme/layouts/NotFound.vue
+++ vuepress/docs/.vuepress/theme/layouts/NotFound.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="notfound-container">
+    <div class="notfound-container" v-if="state.display">
         <img src="/logomark.svg" />       
 
         <h1>404 Not Found</h1>
@@ -10,10 +10,22 @@
     </div>
 </template>
 
-<script lang="ts">
-if (window.document.referrer !== window.document.location.href){
-    // Attempt to retrieve from server instead of vue route
-    window.document.location = window.document.location.href;
-}
+<script setup>
+import { onBeforeMount, reactive } from 'vue'
+
+const state = reactive({ display: false })
+
+onBeforeMount(() => {
+    if (window.document.referrer !== window.document.location.href){
+        // Attempt to retrieve from server instead of vue route
+        // This is required because relative links are handled as vue routes by vuepress - however they may point to another sub-site on the same domain.
+        window.document.location = window.document.location.href;
+        state.display = false;
+    } else {
+        state.display = true;
+    }
+})
+
+
 </script>
 
