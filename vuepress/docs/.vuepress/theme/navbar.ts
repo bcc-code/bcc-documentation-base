7105ee9a0a1ce866c79fbbb58c120d2ac82f6abe
Oz Okun <112382001+Oz-Okun@users.noreply.github.com>
2024-05-23 11:40:58 +0200

    Add a Logout button

--- vuepress/docs/.vuepress/theme/navbar.ts
+++ vuepress/docs/.vuepress/theme/navbar.ts
@@ -2,6 +2,10 @@ import type { NavbarConfig } from '@vuepress/theme-default'
 
 export const navbar: NavbarConfig = [
     {
+        text: 'Logout',
+        link: '/logout',
+    },
+    {
         text: 'Design System',
         link: 'https://developer.bcc.no/bcc-design',
     },
