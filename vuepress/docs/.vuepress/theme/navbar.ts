43e369a5a9ee061793b58e86547c05bea7c14803
Reng van Oord <rvanoord@users.noreply.github.com>
2025-02-24 16:33:21 +0100

    Update link in menu

--- vuepress/docs/.vuepress/theme/navbar.ts
+++ vuepress/docs/.vuepress/theme/navbar.ts
@@ -3,6 +3,6 @@ import { navbar } from "vuepress-theme-hope";
 export default navbar([
     {
         text: 'Design System',
-        link: 'https://developer.bcc.no/bcc-design',
+        link: '/bcc-design',
     },
 ]);
